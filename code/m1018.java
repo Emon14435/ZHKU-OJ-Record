061b48b8301817b0abe9b9161fd6f71ba96e1ef7
Emon.Evo <2945921033@qq.com>
2021-12-22 15:49:13 +0800

    m1018（尚未完成）
    ZHKU-OJ第1018题还没做成，，，，，，，

---
+++ code/m1018.java
@@ -0,0 +1,21 @@
+package OJ;
+
+import java.util.Scanner;
+
+public class m1018 {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()){
+            n = sc.nextInt();
+            for()
+            for(int i = n; i <= n; i--) {
+                System.out.printf("* ");
+            }
+            System.out.println("*");
+            for(int j = 0; j<=n-1;j++) {
+                System.out.print(" ");
+            }
+        }
+    }
+}
