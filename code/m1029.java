87ee869a44680f218eae0ab6d4e76d8371215821
Emon.Evo <2945921033@qq.com>
2021-12-23 19:13:02 +0800

    1018x修改

---
+++ code/m1029.java
@@ -0,0 +1,25 @@
+package OJ;
+
+import java.util.Scanner;
+
+public class m1029 {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+
+        while(in.hasNextInt()) {
+            int n=in.nextInt();
+            int N=1;
+            System.out.println(N);
+            for(int i=1;i<n;i++) {
+                for(int j=0;j<=i;j++) {
+                    N+=1;
+                    if(j==i)
+                        System.out.println(N);
+                    else
+                        System.out.print(N+" ");
+                }
+            }
+        }
+    }
+}
