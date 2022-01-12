b85f3fc4bd16b735a4c4078623dee276f20438ec
Emon14435 <2945921033@qq.com>
2022-01-12 15:47:48 +0800

    1018完成

--- code/m1018.java
+++ code/m1018.java
@@ -8,15 +8,14 @@ public class m1018 {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextInt()) {
             n = sc.nextInt();
-            for (int z = 0; z < n; z++) {
-                for (int i = 0; i <= n; i++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print("  ");//题目是两个空格，大概就是（行数-1）*2，这里用两个空格代替
+                }
+                for (int k = 0; k < n - 1; k++) {
                     System.out.print("* ");
                 }
                 System.out.println("*");
-                for (int j = 0; j <= n - 1; j++) {
-                    System.out.print(" ");
-                    //WARNING:这里没完工
-                }
             }
         }
     }
