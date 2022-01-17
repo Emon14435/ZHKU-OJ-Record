b41cbe6ece42cd8b2c4103de7f367285c048b91f
Emon14435 <2945921033@qq.com>
2022-01-17 20:27:51 +0800

    Revert "根据IDEA的提示做了一点改动，但是建议使用上一个版本进行算法的做"
    This reverts commit a43645c20ef3db196658d4358b2ab2ab3400c167.

--- code/m1061.java
+++ code/m1061.java
@@ -3,12 +3,12 @@ package OJ;
 public class m1061 {
     //Accepted
     public static void main(String[] args) {
-        StringBuilder o = new StringBuilder();
+        String o = "";
         for (int i = 100; i <= 300; i++) {
           if (i % 3 != 0 && i % 5 != 0) {
-            o.append(i).append(" ");
+            o += i + " ";
           }
         }
-        System.out.println(o.toString().trim());
+        System.out.println(o.trim());
       }
 }
